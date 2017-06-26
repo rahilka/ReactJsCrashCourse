4c1db33ad71e75a6554c1595d5a11b85a8956100
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-06-26 12:11:44 +0200

    Add new project

--- hello-world/src/components/AddProject.js
+++ hello-world/src/components/AddProject.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 class AddProject extends Component {
 	
+	constructor() {
+		super();
+		this.state = {
+			newProject:{}
+		}
+		
+	}
+	
 	static defaultProps = {
 	
 		categories: ['Web Design', 'Mobile Development', 'Web Development']
@@ -9,20 +17,30 @@ class AddProject extends Component {
 	}
 	
 	handleSubmit(e) {
-		console.log('handle submit...');
+		if(this.refs.title.value === '') {
+			alert('Title is required!');
+		} else {
+			this.setState({newProject: {
+				title: this.refs.title.value,
+				category: this.refs.category.value
+			}}, function() {
+				//console.log('STATE:', this.state);
+				this.props.addProject(this.state.newProject);
+			});
+		}
 		e.preventDefault();
 	}
 	
   render() {
 	  
 	  let categoryOptions = this.props.categories.map(category => {
-		  return <option key={category} value="category">{category}</option>
+		  return <option key={category} value={category}>{category}</option>
 	  });
 	  
     return (
       <div>
 		<h3>Add Project</h3>
-		<form onSubmit={this.handleSubmit}>
+		<form onSubmit={this.handleSubmit.bind(this)}>
 			<div>
 				<label>Title</label><br />
 				<input type="text" ref="title" />
